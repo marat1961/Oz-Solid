1472d79cc3cdaf9bfb943b7c59a4c95bb6050d57
marat1961 <marat.sh.1961@gmail.com>
2020-12-14 23:57:10 +0700

    Add method signatures for the svg builder

--- src/Oz.Solid.Svg.pas
+++ src/Oz.Solid.Svg.pas
@@ -28,7 +28,86 @@ uses
 
 {$T+}
 
+{$Region 'TsdSvg'}
+
+type
+
+  TsdSvg = class
+  type
+    TsvgRoot = class
+    end;
+    TRect = class
+    end;
+    TLine = class
+    end;
+    TPolyline = class
+    end;
+    TPolygon = class
+    end;
+    TPath = class
+    end;
+  private
+    FWidth: Double;
+    FHeight: Double;
+  public
+    function Init(Width, Height: Double): TsvgRoot;
+    // <rect x="10" y="10" width="30" height="30"
+    //  stroke="black" fill="transparent" stroke-width="5"/>
+    function Rect(x, y, Width, Height: Double): TRect;
+    // <line x1="10" x2="50" y1="110" y2="150"
+    //  stroke="orange" fill="transparent" stroke-width="5"/>
+    function Line(x1, y1, x2, y2: Double): TLine;
+    // <polyline points="60 110 65 120 70 115 75 130 80 125 85 140 90 135 95 150 100 145"
+    //  stroke="orange" fill="transparent" stroke-width="5"/>
+    function Polyline: TPolyline;
+    // polygon points="50 160 55 180 70 180 60 190 65 205 50 195 35 205 40 190 30 180 45 180"
+    //  stroke="green" fill="transparent" stroke-width="5"/>
+    function Polygon: TPolygon;
+    // <path d="M20,230 Q40,205 50,230 T90,230" fill="none" stroke="blue" stroke-width="5"/>
+    function Path: TPath;
+    function ToString: string;
+  end;
+
+{$EndRegion}
+
 implementation
+
+{ TsdSvg }
+
+function TsdSvg.Init(Width, Height: Double): TsvgRoot;
+begin
+
+end;
+
+function TsdSvg.Line(x1, y1, x2, y2: Double): TLine;
+begin
+
+end;
+
+function TsdSvg.Path: TPath;
+begin
+
+end;
+
+function TsdSvg.Polygon: TPolygon;
+begin
+
+end;
+
+function TsdSvg.Polyline: TPolyline;
+begin
+
+end;
+
+function TsdSvg.Rect(x, y, Width, Height: Double): TRect;
+begin
+
+end;
+
+function TsdSvg.ToString: string;
+begin
+
+end;
 
 end.
 
