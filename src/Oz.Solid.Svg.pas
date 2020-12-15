fd7aa04f4fb94a58bb04835c0bc1823a150af283
marat1961 <marat.sh.1961@gmail.com>
2020-12-15 23:36:47 +0700

    Added simple svg builder for generate svg file

--- src/Oz.Solid.Svg.pas
+++ src/Oz.Solid.Svg.pas
@@ -22,49 +22,105 @@ interface
 {$Region 'Uses'}
 
 uses
-  System.Classes, System.SysUtils, Oz.Solid.VectorInt;
+  System.Classes, System.SysUtils, System.Contnrs,
+  Oz.Solid.VectorInt, Oz.Solid.Types;
 
 {$EndRegion}
 
 {$T+}
 
-{$Region 'TsdSvg'}
+{$Region 'TsvgBuilder'}
 
 type
 
-  TsdSvg = class
-  type
-    TsvgRoot = class
-    end;
-    TRect = class
-    end;
-    TLine = class
-    end;
-    TPolyline = class
-    end;
-    TPolygon = class
-    end;
-    TPath = class
-    end;
+  // Shapes and text context elements
+  TsvgShape = class
+    // presentation attribute that defines the color
+    function Fill(const color: string): TsvgShape;
+    // presentation attribute defining the color used to paint the outline of the shape
+    function Stroke(const color: string): TsvgShape;
+    // presentation attribute defining the width of the stroke to be applied to the shape
+    function StrokeWidth(const Width: Double): TsvgShape;
+  end;
+
+  // The <rect> element draws a rectangle on the screen.
+  TsvgRect = class(TsvgShape)
+    // The x radius of the corners of the rectangle
+    function Rx(const radius: Double): TsvgRect;
+    // The y radius of the corners of the rectangle
+    function Ry(const radius: Double): TsvgRect;
+  end;
+
+  // The <circle> element draws a circle on the screen.
+  TsvgCircle = class(TsvgShape);
+
+  // An <ellipse> is a more general form of the <circle> element.
+  TsvgEllipse = class(TsvgShape);
+
+  // The <line> element is an SVG basic shape used
+  // to create a line connecting two points.
+  TsvgLine = class(TsvgShape);
+
+  TsvgPoly = class(TsvgShape)
+    function Point(const x, y: Double): TsvgPoly; overload;
+    function Point(const pt: T2dPoint): TsvgPoly; overload;
+    function Point(const pt: T2i): TsvgPoly; overload;
+    function Points(const points: T2dPoints): TsvgPoly;
+  end;
+
+  // A <polyline> is a group of connected straight lines.
+  TsvgPolyline = class(TsvgPoly);
+
+  // The <polygon> element defines a closed shape consisting of a set
+  // of connected straight line segments.
+  // The last point is connected to the first point.
+  TsvgPolygon = class(TsvgPoly);
+
+  // The <path> SVG element is the generic element to define a shape.
+  // All the basic shapes can be created with a path element.
+  TsvgPath = class(TsvgShape)
+    // op
+    //   MoveTo: M, m
+    //   LineTo: L, l, H, h, V, v
+    //   Cubic Bezier Curve: C, c, S, s
+    //   Quadratic Bezier Curve: Q, q, T, t
+    //   Elliptical Arc Curve: A, a
+    //   ClosePath: Z, z
+    function D(op: Char; const points: T2dPoints): TsvgPath;
+  end;
+
+  TsvgText = class(TsvgShape)
+    function Cls(const value: string): TsvgText;
+  end;
+
+  TViewBox = record
+    min_x: Double;
+    min_y: Double;
+    width: Double;
+    height: Double;
+  end;
+
+  TsvgBuilder = class
   private
     FWidth: Double;
     FHeight: Double;
+    FViewBox: TViewBox;
+    FShapes: TObjectList;
   public
-    function Init(Width, Height: Double): TsvgRoot;
-    // <rect x="10" y="10" width="30" height="30"
-    //  stroke="black" fill="transparent" stroke-width="5"/>
-    function Rect(x, y, Width, Height: Double): TRect;
-    // <line x1="10" x2="50" y1="110" y2="150"
-    //  stroke="orange" fill="transparent" stroke-width="5"/>
-    function Line(x1, y1, x2, y2: Double): TLine;
-    // <polyline points="60 110 65 120 70 115 75 130 80 125 85 140 90 135 95 150 100 145"
-    //  stroke="orange" fill="transparent" stroke-width="5"/>
-    function Polyline: TPolyline;
-    // polygon points="50 160 55 180 70 180 60 190 65 205 50 195 35 205 40 190 30 180 45 180"
-    //  stroke="green" fill="transparent" stroke-width="5"/>
-    function Polygon: TPolygon;
-    // <path d="M20,230 Q40,205 50,230 T90,230" fill="none" stroke="blue" stroke-width="5"/>
-    function Path: TPath;
+    constructor Create(width, height: Double);
+    destructor Destroy; overload;
+    // The viewBox attribute defines the position and dimension,
+    // in user space, of an SVG viewport.
+    function ViewBox(min_x, min_y, width, height: Double): TsvgBuilder;
+    function Rect(x, y, width, height: Double): TsvgRect;
+    function Circle(cx, cy, r: Double): TsvgCircle;
+    function Ellipse(cx, cy, rx, ry: Double): TsvgEllipse;
+    function Line(x1, y1, x2, y2: Double): TsvgLine;
+    function Polyline: TsvgPolyline;
+    function Polygon: TsvgPolygon;
+    function Path: TsvgPath;
+    // The SVG <text> element draws a graphics element consisting of text.
+    function Text(x, y: Double; const text, cls: string): TsvgText;
     function ToString: string;
   end;
 
@@ -72,42 +128,79 @@ type
 
 implementation
 
-{ TsdSvg }
+{$Region 'TsvgBuilder'}
 
-function TsdSvg.Init(Width, Height: Double): TsvgRoot;
+constructor TsvgBuilder.Create(width, height: Double);
+begin
+  inherited Create;
+  FWidth := width;
+  FHeight := height;
+  FShapes := TObjectList.Create;
+end;
+
+destructor TsvgBuilder.Destroy;
+begin
+  FShapes.Free;
+  inherited;
+end;
+
+function TsvgBuilder.ViewBox(min_x, min_y, width, height: Double): TsvgBuilder;
+begin
+  FViewBox.min_x := min_x;
+  FViewBox.min_y := min_y;
+  FViewBox.width := width;
+  FViewBox.height := height;
+end;
+
+function TsvgBuilder.Rect(x, y, width, height: Double): TsvgRect;
 begin
 
 end;
 
-function TsdSvg.Line(x1, y1, x2, y2: Double): TLine;
+function TsvgBuilder.Circle(cx, cy, r: Double): TsvgCircle;
 begin
 
 end;
 
-function TsdSvg.Path: TPath;
+function TsvgBuilder.Ellipse(cx, cy, rx, ry: Double): TsvgEllipse;
 begin
 
 end;
 
-function TsdSvg.Polygon: TPolygon;
+function TsvgBuilder.Line(x1, y1, x2, y2: Double): TsvgLine;
 begin
 
 end;
 
-function TsdSvg.Polyline: TPolyline;
+function TsvgBuilder.Polyline: TsvgPolyline;
 begin
 
 end;
 
-function TsdSvg.Rect(x, y, Width, Height: Double): TRect;
+function TsvgBuilder.Polygon: TsvgPolygon;
 begin
 
 end;
 
-function TsdSvg.ToString: string;
+function TsvgBuilder.Path: TsvgPath;
 begin
 
 end;
+
+function TsvgBuilder.ToString: string;
+var
+  sb: TStringBuilder;
+begin
+  sb := TStringBuilder.Create;
+  try
+
+    Result := sb.ToString;
+  finally
+    sb.Free;
+  end;
+end;
+
+{$EndRegion}
 
 end.
 
