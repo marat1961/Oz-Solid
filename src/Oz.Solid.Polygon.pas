0b528645b0300488dd93154edd05a335d9ea7b72
marat1961 <marat.sh.1961@gmail.com>
2021-02-25 09:10:28 +0700

    implementation of work logic

--- src/Oz.Solid.Polygon.pas
+++ src/Oz.Solid.Polygon.pas
@@ -33,6 +33,8 @@ uses
 
 const
   PMAX = 1000;  // Max # of pts in polygon
+  EXIT_SUCCESS = 0;
+  EXIT_FAILURE = 1;
 
 type
   tInFlag = (Pin, Qin, Unknown);
@@ -71,96 +73,106 @@ type
 
 {$EndRegion}
 
-procedure ClosePostscript();
-procedure PrintSharedSeg(p, q: tPointd);
-function Dot(a, b: tPointi): Double;
-function AreaSign(a, b, c: tPointi): Integer;
-function SegSegInt(a, b, c, d: tPointi; p, q: tPointd): char;
-function ParallelInt(a, b, c, d: tPointi; p, q: tPointd): char;
-function Between(a, b, c: tPointi): Boolean;
-procedure Assigndi(p: tPointd; a: tPointi);
-procedure SubVec(a, b, c: tPointi);
-function LeftOn(a, b, c: tPointi): Boolean;
-function Left(a, b, c: tPointi): Boolean;
-procedure PrintPoly(n: Integer; P: tPolygoni);
+{$Region 'TPolyBuilder'}
 
-// P has n vertices, Q has m vertices.
-procedure ConvexIntersect(P, Q: tPolygoni; n, m: Integer);
+  TPolyBuilder = record
+  private
+    io: TsvgIO;
+    procedure ClosePostscript();
+    procedure PrintSharedSeg(p, q: tPointd);
+    function Dot(a, b: tPointi): Double;
+    function AreaSign(a, b, c: tPointi): Integer;
+    function SegSegInt(a, b, c, d: tPointi; p, q: tPointd): Char;
+    function ParallelInt(a, b, c, d: tPointi; p, q: tPointd): Char;
+    function Between(a, b, c: tPointi): Boolean;
+    procedure Assigndi(p: tPointd; a: tPointi);
+    procedure SubVec(a, b, c: tPointi);
+    function LeftOn(a, b, c: tPointi): Boolean;
+    function Left(a, b, c: tPointi): Boolean;
+    procedure PrintPoly(n: Integer; P: tPolygoni);
 
-function InOut(p: tPointd; inflag: tInFlag; aHB, bHA: Integer): tInFlag;
-function Advance(a: Integer; var aa: PInteger;
-  n: Integer; inside: Boolean; v: tPointi): Integer;
-procedure OutputPolygons();
+    function InOut(p: tPointd; inflag: tInFlag; aHB, bHA: Integer): tInFlag;
+    function Advance(a: Integer; var aa: Integer;
+      n: Integer; inside: Boolean; v: tPointi): Integer;
+    procedure OutputPolygons();
+  public
+    procedure From(const filename: string);
+    procedure Free;
+    // P has n vertices, Q has m vertices.
+    function ConvexIntersect(P, Q: tPolygoni; n, m: Integer): Integer;
+  end;
+
+{$EndRegion}
 
 implementation
 
 {$Region ''}
 
-procedure ClosePostscript();
+procedure TPolyBuilder.ClosePostscript();
 begin
 //  printf("closepath stroke\n");
 //  printf("showpage\n%%%%EOF\n");
 end;
 
-procedure PrintSharedSeg(p, q: tPointd);
+procedure TPolyBuilder.PrintSharedSeg(p, q: tPointd);
 begin
 //  printf("%%A int B:\n");
-//  printf("%8.2lf %8.2lf moveto\n", p[X], p[Y] );
-//  printf("%8.2lf %8.2lf lineto\n", q[X], q[Y] );
+//  printf("%8.2lf %8.2lf moveto\n", p.x, p.y );
+//  printf("%8.2lf %8.2lf lineto\n", q.x, q.y );
 //  ClosePostscript();
 end;
 
-function Dot(a, b: tPointi): Double;
+function TPolyBuilder.Dot(a, b: tPointi): Double;
 begin
 
 end;
 
-function AreaSign(a, b, c: tPointi): Integer;
+function TPolyBuilder.AreaSign(a, b, c: tPointi): Integer;
 begin
 
 end;
 
-function SegSegInt(a, b, c, d: tPointi; p, q: tPointd): char;
+function TPolyBuilder.SegSegInt(a, b, c, d: tPointi; p, q: tPointd): Char;
 begin
 
 end;
 
-function ParallelInt(a, b, c, d: tPointi; p, q: tPointd): char;
+function TPolyBuilder.ParallelInt(a, b, c, d: tPointi; p, q: tPointd): Char;
 begin
 
 end;
 
-function Between(a, b, c: tPointi): Boolean;
+function TPolyBuilder.Between(a, b, c: tPointi): Boolean;
 begin
 
 end;
 
-procedure Assigndi(p: tPointd; a: tPointi);
+procedure TPolyBuilder.Assigndi(p: tPointd; a: tPointi);
 begin
 
 end;
 
-procedure SubVec(a, b, c: tPointi);
+procedure TPolyBuilder.SubVec(a, b, c: tPointi);
 begin
 
 end;
 
-function LeftOn(a, b, c: tPointi): Boolean;
+function TPolyBuilder.LeftOn(a, b, c: tPointi): Boolean;
 begin
 
 end;
 
-function Left(a, b, c: tPointi): Boolean;
+function TPolyBuilder.Left(a, b, c: tPointi): Boolean;
 begin
 
 end;
 
-procedure PrintPoly(n: Integer; P: tPolygoni);
+procedure TPolyBuilder.PrintPoly(n: Integer; P: tPolygoni);
 begin
 
 end;
 
-procedure ConvexIntersect(P, Q: tPolygoni; n, m: Integer);
+function TPolyBuilder.ConvexIntersect(P, Q: tPolygoni; n, m: Integer): Integer;
 var
   a, b: Integer;       // indices on P and Q (resp.)
   a1, b1: Integer;     // a-1, b-1 (resp.)
@@ -174,7 +186,7 @@ var
   aa, ba: Integer;     // # advances on a & b indices (after 1st inter.)
   FirstPoint: Boolean; // Is this the first point? (used to initialize).
   p0: tPointd;         // The first point.
-  code: Integer;       // SegSegInt return code.
+  code: Char;          // SegSegInt return code.
 begin
   Origin.x := 0;
   Origin.y := 0;
@@ -184,7 +196,8 @@ begin
   FirstPoint := TRUE;
 
   repeat
-    // Dbp('%%Before Advances:a=%d, b=%d; aa=%d, ba=%d; inflag=%d\n', a, b, aa, ba, inflag);*/
+    io.Dbp('%%Before Advances:a=%d, b=%d; aa=%d, ba=%d; inflag=%d',
+      [a, b, aa, ba, Ord(inflag)]);
     // Computations of key variables.
     a1 := (a + n - 1) mod n;
     b1 := (b + m - 1) mod m;
@@ -195,35 +208,38 @@ begin
     cross := AreaSign( Origin, A_, B_);
     aHB := AreaSign(Q[b1], Q[b], P[a]);
     bHA := AreaSign(P[a1], P[a], Q[b]);
-    Dbp('%%cross=%d, aHB=%d, bHA=%d\n', [cross, aHB, bHA]);
+    io.Dbp('%%cross=%d, aHB=%d, bHA=%d', [cross, aHB, bHA]);
 
     // If A_ & B_ intersect, update inflag.
     code := SegSegInt(P[a1], P[a], Q[b1], Q[b], p_, q_);
-    Dbp('%%SegSegInt: code = %c\n', [code]);
+    io.Dbp('%%SegSegInt: code = %c', [code]);
     if (code = '1') or (code = 'v') then
     begin
        if (inflag = Unknown) and FirstPoint then
        begin
           aa := 0; ba := 0;
           FirstPoint := FALSE;
-          p0[X] := p_[X];
-          p0[Y] := p_[Y];
-          Dbp('%8.2lf %8.2lf moveto\n', [p0[X], p0[Y]]);
+          p0.x := p_.x;
+          p0.y := p_.y;
+          io.Dbp('%8.2lf %8.2lf moveto', [p0.x, p0.y]);
        end;
        inflag := InOut(p_, inflag, aHB, bHA);
-       Dbp('%%InOut sets inflag=%d\n', [inflag]);
+       io.Dbp('%%InOut sets inflag=%d', [Ord(inflag)]);
     end;
 
     // Advance rule
 
     // Special case: A_ & B_ overlap and oppositely oriented.
     if (code = 'e' ) and (Dot(A_, B_) < 0) then
-      PrintSharedSeg( p_, q_ ), exit(EXIT_SUCCESS);
+    begin
+      PrintSharedSeg( p_, q_);
+      exit(EXIT_SUCCESS);
+    end;
 
     // Special case: A_ & B_ parallel and separated.
     if (cross = 0) and (aHB < 0) and (bHA < 0) then
     begin
-      Dbp('%%P and Q are disjoint.\n');
+      io.Dbp('%%P and Q are disjoint.');
       exit(EXIT_SUCCESS);
     end
     // Special case: A_ & B_ collinear.
@@ -231,9 +247,9 @@ begin
     begin
       // Advance but do not output point.
       if inflag = Pin then
-        b := Advance(b, @ba, m, inflag = Qin, Q[b])
+        b := Advance(b, ba, m, inflag = Qin, Q[b])
       else
-        a := Advance(a, @aa, n, inflag = Pin, P[a]);
+        a := Advance(a, aa, n, inflag = Pin, P[a]);
    end
    // Generic cases.
    else if cross >= 0 then
@@ -242,7 +258,7 @@ begin
        a := Advance( a, &aa, n, inflag = Pin, P[a])
      else
        b := Advance( b, &ba, m, inflag = Qin, Q[b]);
-    end;
+    end
     else // if ( cross < 0 )
     begin
       if aHB > 0 then
@@ -250,18 +266,19 @@ begin
       else
         a := Advance(a, &aa, n, inflag = Pin, P[a]);
     end;
-    Dbp('%%After advances:a=%d, b=%d; aa=%d, ba=%d; inflag=%d\n', a, b, aa, ba, inflag);
+    io.Dbp('%%After advances:a=%d, b=%d; aa=%d, ba=%d; inflag=%d',
+      [a, b, aa, ba, Ord(inflag)]);
 
-  // Quit when both adv. indices have cycled, or one has cycled twice. */
-  until ( ((aa < n) or (ba < m)) and (aa < 2*n) and (ba < 2*m) );
+    // Quit when both adv. indices have cycled, or one has cycled twice.
+  until not (((aa < n) or (ba < m)) and (aa < 2 * n) and (ba < 2 * m));
 
   if not FirstPoint then
-    // If at least one point output, close up. */
-    Dbp('%8.2lf %8.2lf lineto\n', p0[X], p0[Y]);
+    // If at least one point output, close up.
+    io.Dbp('%8.2lf %8.2lf lineto', [p0.x, p0.y]);
 
-  // Deal with special cases: not implemented. */
+  // Deal with special cases: not implemented.
   if inflag = Unknown then
-    Dbp('%%The boundaries of P and Q do not cross.\n');
+    io.Dbp('%%The boundaries of P and Q do not cross.');
 end;
 
 {$EndRegion}
