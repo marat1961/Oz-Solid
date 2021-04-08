eb382a669180fe1fccac946d39390b455da40d52
marat1961 <marat.sh.1961@gmail.com>
2021-04-08 23:44:12 +0700

    refactoring

--- src/Oz.Solid.Matrix.pas
+++ src/Oz.Solid.Matrix.pas
@@ -31,21 +31,18 @@ type
 
   TVector = record
   private
-    FTuple: TArray<Double>;
+    FItems: TArray<Double>;
     function GetItem(i: Integer): Double; inline;
     procedure SetItem(i: Integer; value: Double); inline;
   public
     // The tuple is length 'size' and the elements are uninitialized.
     constructor From(size: Integer); overload;
-    // For 0 <= d <= size, element d is 1 and all others are zero.
+    // For d in [0 .. numRows - 1], element d is 1 and all others are zero.
     // If d is invalid, the zero vector is created.
     // This is a convenience for creating the standard Euclidean basis vectors;
     // see also MakeUnit(Integer, Integer) and Unit(Integer, Integer).
     constructor From(size, d: Integer); overload;
     // Member access. SetSize(Integer) does not initialize the tuple.
-    // The first operator[] returns a const reference rather than
-    // a Double value. This supports writing via standard file operations
-    // that require a const pointer to data.
     procedure SetSize(size: Integer); inline;
     function GetSize: Integer; inline;
     // All components are 0.
@@ -75,7 +72,6 @@ type
   TMatrix = record
   private
     // The matrix is stored as a 1-dimensional array.
-    // The convention of row-major or column-major is your choice.
     FNumRows: Integer;
     FNumCols: Integer;
     FElements: TArray<Double>;
@@ -85,17 +81,15 @@ type
     // The table is length = numRows * numCols
     // and the elements are initialized to zero.
     constructor From(numRows, numCols: Integer); overload;
-    // For 0 <= r < numRows and 0 <= c < numCols,
+    // For r in [0 .. numRows - 1] and c in [0 .. numCols - 1]
     // element (r, c) is 1 and all others are 0.
     // If either of r or c is invalid, the zero matrix is created.
     // This is a convenience for creating the standard Euclidean basis matrices;
     // see also MakeUnit(Integer,Integer) and Unit(Integer,Integer).
     constructor From(numRows, numCols, r, c: Integer); overload;
     // Member access for which the storage representation is transparent.
-    // The matrix entry in row r and column c is A(r,c).  The first
-    // operator() returns a const reference rather than a Double value.
-    // This supports writing via standard file operations that require a
-    // const pointer to data.
+
+    // The matrix entry in row r and column c is A(r,c).
     procedure SetSize(numRows, numCols: Integer);
     procedure GetSize(var numRows, numCols: Integer); inline;
 
@@ -111,6 +105,12 @@ type
     procedure MakeUnit(r, c: Integer);
     // Result := Self * M
     function Multiply(const M: TMatrix): TMatrix;
+
+    property NumRows: Integer read FNumRows;
+    property NumCols: Integer read FNumCols;
+    // Returns a total number of Elements.
+    property NumElements: Integer read GetNumElements;
+    // Returns a reference to Double value.
     property Element[r, c: Integer]: PDouble read GetElement; default;
   end;
 
@@ -134,35 +134,35 @@ end;
 procedure TVector.SetSize(size: Integer);
 begin
   Assert(size >= 0, 'Invalid size.');
-  SetLength(FTuple, size);
+  SetLength(FItems, size);
 end;
 
 function TVector.GetSize: Integer;
 begin
-  Result := Length(FTuple);
+  Result := Length(FItems);
 end;
 
 function TVector.GetItem(i: Integer): Double;
 begin
-  Result := FTuple[i];
+  Result := FItems[i];
 end;
 
 procedure TVector.SetItem(i: Integer; Value: Double);
 begin
-  FTuple[i] := Value;
+  FItems[i] := Value;
 end;
 
 procedure TVector.MakeZero;
 var
   i: Integer;
 begin
-  for i := 0 to High(FTuple) do
-    FTuple[i] := 0.0;
+  for i := 0 to High(FItems) do
+    FItems[i] := 0.0;
 end;
 
 function TVector.Zero(size: Integer): TVector;
 begin
-  SetLength(Result.FTuple, size);
+  SetLength(Result.FItems, size);
   Result.MakeZero;
 end;
 
@@ -170,16 +170,16 @@ procedure TVector.MakeUnit(d: Integer);
 var
   i: Integer;
 begin
-  for i := 0 to High(FTuple) do
+  for i := 0 to High(FItems) do
     if i = d then
-      FTuple[i] := 1.0
+      FItems[i] := 1.0
     else
-      FTuple[i] := 0.0;
+      FItems[i] := 0.0;
 end;
 
 function TVector.GetUnit(size, d: Integer): TVector;
 begin
-  SetLength(Result.FTuple, size);
+  SetLength(Result.FItems, size);
   Result.MakeUnit(d);
 end;
 
@@ -190,8 +190,8 @@ begin
   n := GetSize;
   Result := n = v.GetSize;
   if Result then
-    for i := 0 to High(FTuple) do
-      if not SameValue(FTuple[i], v.FTuple[i]) then
+    for i := 0 to High(FItems) do
+      if not SameValue(FItems[i], v.FItems[i]) then
         exit(False);
 end;
 
@@ -199,8 +199,8 @@ function TVector.Negative: TVector;
 var
   i: Integer;
 begin
-  for i := 0 to High(FTuple) do
-    Result.FTuple[i] := FTuple[i];
+  for i := 0 to High(FItems) do
+    Result.FItems[i] := FItems[i];
 end;
 
 function TVector.Plus(const v: TVector): TVector;
@@ -208,8 +208,8 @@ var
   i: Integer;
 begin
   Assert(GetSize = v.GetSize, 'Mismatched sizes');
-  for i := 0 to High(FTuple) do
-    Result.FTuple[i] := FTuple[i] + v.FTuple[i];
+  for i := 0 to High(FItems) do
+    Result.FItems[i] := FItems[i] + v.FItems[i];
 end;
 
 function TVector.Minus(const v: TVector): TVector;
@@ -217,16 +217,16 @@ var
   i: Integer;
 begin
   Assert(GetSize = v.GetSize, 'Mismatched sizes');
-  for i := 0 to High(FTuple) do
-    Result.FTuple[i] := FTuple[i] - v.FTuple[i];
+  for i := 0 to High(FItems) do
+    Result.FItems[i] := FItems[i] - v.FItems[i];
 end;
 
 function TVector.Scale(s: Double): TVector;
 var
   i: Integer;
 begin
-  for i := 0 to High(FTuple) do
-    Result.FTuple[i] := FTuple[i] * s;
+  for i := 0 to High(FItems) do
+    Result.FItems[i] := FItems[i] * s;
 end;
 
 {$EndRegion}
@@ -305,7 +305,7 @@ end;
 procedure TMatrix.MakeUnit(r, c: Integer);
 begin
   CheckIndex(r, c);
-  MakeZero();
+  MakeZero;
   Element[r, c]^ := 1.0;
 end;
 
@@ -316,10 +316,10 @@ var
 begin
   Assert(Self.GetNumCols = M.GetNumRows, 'Mismatched sizes');
   Result := TMatrix.From(Self.GetNumRows, M.GetNumCols);
-  numCommon := Self.GetNumCols();
-  for r := 0 to result.GetNumRows - 1 do
+  numCommon := Self.GetNumCols;
+  for r := 0 to Result.GetNumRows - 1 do
   begin
-    for c := 0 to result.GetNumCols - 1 do
+    for c := 0 to Result.GetNumCols - 1 do
     begin
       Result.Element[r, c]^ := 0.0;
       for i := 0 to numCommon - 1 do
