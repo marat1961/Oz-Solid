3c1960936653105444abc75eb828aee982abc0a4
marat1961 <marat.sh.1961@gmail.com>
2021-04-08 23:21:21 +0700

    implementing methods for working with vectors and matrices

--- src/Oz.Solid.Matrix.pas
+++ src/Oz.Solid.Matrix.pas
@@ -20,9 +20,12 @@ unit Oz.Solid.Matrix;
 interface
 
 uses
-  System.Math;
+  System.SysUtils, System.Math;
 
 type
+
+  EMatrixError = class(Exception)
+  end;
 
 {$Region 'TVector'}
 
@@ -69,8 +72,46 @@ type
 
 {$Region 'TMatrix'}
 
-  TMatrix = class
+  TMatrix = record
+  private
+    // The matrix is stored as a 1-dimensional array.
+    // The convention of row-major or column-major is your choice.
+    FNumRows: Integer;
+    FNumCols: Integer;
+    FElements: TArray<Double>;
+    function GetElement(r, c: Integer): PDouble; inline;
+    procedure CheckIndex(r, c: Integer); inline;
   public
+    // The table is length = numRows * numCols
+    // and the elements are initialized to zero.
+    constructor From(numRows, numCols: Integer); overload;
+    // For 0 <= r < numRows and 0 <= c < numCols,
+    // element (r, c) is 1 and all others are 0.
+    // If either of r or c is invalid, the zero matrix is created.
+    // This is a convenience for creating the standard Euclidean basis matrices;
+    // see also MakeUnit(Integer,Integer) and Unit(Integer,Integer).
+    constructor From(numRows, numCols, r, c: Integer); overload;
+    // Member access for which the storage representation is transparent.
+    // The matrix entry in row r and column c is A(r,c).  The first
+    // operator() returns a const reference rather than a Double value.
+    // This supports writing via standard file operations that require a
+    // const pointer to data.
+    procedure SetSize(numRows, numCols: Integer);
+    procedure GetSize(var numRows, numCols: Integer); inline;
+
+    function GetNumRows: Integer; inline;
+    function GetNumCols: Integer; inline;
+    function GetNumElements: Integer; inline;
+
+    (* Special matrices *)
+
+    // All components are 0.
+    procedure MakeZero;
+    // Component (r,c) is 1, all others zero.
+    procedure MakeUnit(r, c: Integer);
+    // Result := Self * M
+    function Multiply(const M: TMatrix): TMatrix;
+    property Element[r, c: Integer]: PDouble read GetElement; default;
   end;
 
 {$EndRegion}
@@ -186,6 +227,108 @@ var
 begin
   for i := 0 to High(FTuple) do
     Result.FTuple[i] := FTuple[i] * s;
+end;
+
+{$EndRegion}
+
+{$Region 'TMatrix'}
+
+constructor TMatrix.From(numRows, numCols: Integer);
+begin
+  SetSize(numRows, numCols);
+end;
+
+constructor TMatrix.From(numRows, numCols, r, c: Integer);
+begin
+  SetSize(numRows, numCols);
+  MakeUnit(r, c);
+end;
+
+procedure TMatrix.GetSize(var numRows, numCols: Integer);
+begin
+  numRows := FNumRows;
+  numCols := FNumCols;
+end;
+
+function TMatrix.GetNumCols: Integer;
+begin
+  Result := FNumCols;
+end;
+
+function TMatrix.GetNumElements: Integer;
+begin
+  Result := Length(FElements);
+end;
+
+function TMatrix.GetNumRows: Integer;
+begin
+  Result := FNumRows;
+end;
+
+procedure TMatrix.CheckIndex(r, c: Integer);
+begin
+  if (Cardinal(r) >= Cardinal(GetNumRows)) or
+     (Cardinal(c) >= Cardinal(GetNumCols)) then
+    raise EMatrixError.Create('Invalid index');
+end;
+
+function TMatrix.GetElement(r, c: Integer): PDouble;
+begin
+  CheckIndex(r, c);
+  Result := @FElements[c + FNumCols * r];
+end;
+
+procedure TMatrix.SetSize(numRows, numCols: Integer);
+begin
+  if (numRows > 0) and (numCols > 0) then
+  begin
+    FNumRows := numRows;
+    FNumCols := numCols;
+    SetLength(FElements, FNumRows * FNumCols);
+  end
+  else
+  begin
+    FNumRows := 0;
+    FNumCols := 0;
+    FElements := [];
+  end;
+end;
+
+procedure TMatrix.MakeZero;
+var
+  i: Integer;
+begin
+  for i := 0 to High(FElements) do
+    FElements[i] := 0.0;
+end;
+
+procedure TMatrix.MakeUnit(r, c: Integer);
+begin
+  CheckIndex(r, c);
+  MakeZero();
+  Element[r, c]^ := 1.0;
+end;
+
+function TMatrix.Multiply(const M: TMatrix): TMatrix;
+var
+  numCommon, r, c, i: Integer;
+  p: PDouble;
+begin
+  Assert(Self.GetNumCols = M.GetNumRows, 'Mismatched sizes');
+  Result := TMatrix.From(Self.GetNumRows, M.GetNumCols);
+  numCommon := Self.GetNumCols();
+  for r := 0 to result.GetNumRows - 1 do
+  begin
+    for c := 0 to result.GetNumCols - 1 do
+    begin
+      Result.Element[r, c]^ := 0.0;
+      for i := 0 to numCommon - 1 do
+      begin
+        p := Result.Element[r, c];
+        p^ := p^ + p^ * M.Element[i, c]^;
+      end;
+    end;
+  end;
 end;
 
 {$EndRegion}
