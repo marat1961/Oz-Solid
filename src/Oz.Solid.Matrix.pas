d325aba0d6cf2e852c948233285d0edc281dc767
marat1961 <marat.sh.1961@gmail.com>
2021-08-17 01:18:02 +0700

    reformat

--- src/Oz.Solid.Matrix.pas
+++ src/Oz.Solid.Matrix.pas
@@ -1,21 +1,21 @@
 (* Oz Solid Library for Pascal
-  * Copyright (c) 2020 Marat Shaimardanov
-  *
-  * This file is part of Oz Solid Library for Pascal
-  * is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * (at your option) any later version.
-  *
-  * This file is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this file. If not, see <https://www.gnu.org/licenses/>.
-*)
-unit Oz.Solid.Matrix;
+ * Copyright (c) 2020 Marat Shaimardanov
+ *
+ * This file is part of Oz Solid Library for Pascal
+ * is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this file. If not, see <https://www.gnu.org/licenses/>.
+ *)
+ unit Oz.Solid.Matrix;
 
 interface
 
